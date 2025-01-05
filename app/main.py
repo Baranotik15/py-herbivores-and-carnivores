4f2819f7ea4342a7b1288acad66623e15867d2f4
RuslanTopal <baranotik15@gmail.com>
2025-01-05 05:18:26 +0000

    del extra space

--- app/main.py
+++ app/main.py
@@ -13,12 +13,12 @@ class Animal:
 
     def __repr__(self) -> str:
         return (f"{{Name: {self.name}, Health: {self.health}, "
-                f" Hidden: {self.hidden}}}")
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
     def hide(self) -> None:
-        self.hidden = not self.hidden
+        self.hidden  = not self.hidden
 
 
 class Carnivore(Animal):
